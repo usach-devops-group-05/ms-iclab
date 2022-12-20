6083d27a37a41d0cc5d261cee2176786cf901d93
Rodrigo G. Higuera M. - Devops <rodrigoghm@gmail.com>
2022-12-20 10:07:08 -0300

    feat: update test estadoPais

--- src/test/java/com/devopsusach2020/rest/RestDataTest.java
+++ src/test/java/com/devopsusach2020/rest/RestDataTest.java
@@ -2,6 +2,7 @@ package com.devopsusach2020.rest;
 
 import com.devopsusach2020.model.Mundial;
 
+import com.devopsusach2020.model.Pais;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +17,30 @@ public class RestDataTest {
         Assertions.assertNotEquals(objMundial.getTotalConfirmed(), 0);
         Assertions.assertNotEquals(objMundial.getTotalDeaths(), 0);
         Assertions.assertEquals(objMundial.getTotalRecovered(), 0);
+    }
+
+    @Test
+    void getTotalPais_thenStatus200() throws Exception  {
+        Pais responsePais = new Pais();
+        responsePais = this.objRestData.getTotalPais("chile");
+        Assertions.assertNotEquals(responsePais.getDeaths(), 0);
+        Assertions.assertNotEquals(responsePais.getConfirmed(), 0);
+        Assertions.assertNotEquals(responsePais.getRecovered(), 0);
+        Assertions.assertNotEquals(responsePais.getActive(), 0);
+        Assertions.assertNotEquals(responsePais.getDate(), "");
+        Assertions.assertEquals(responsePais.getMensaje(), "ok");
+        Assertions.assertEquals(responsePais.getCountry(), "chile");
+    }
+
+    @Test
+    void getTotalPais_thenStatus500() throws Exception  {
+        Pais responsePais = new Pais();
+        try{
+            responsePais = this.objRestData.getTotalPais("eeuu");
+        } catch(Exception e) {
+            responsePais=null;
+        }
+
+        Assertions.assertNull(responsePais);
     }
 }
